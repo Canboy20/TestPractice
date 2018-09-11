3a8ef67f4dbec3159e8aef4031ce18f36cba9085
IRFAN CAN <irfancan.atay@starcut.com>
2018-09-11 23:45:05 +0300

    Visa Card Activity created

---
+++ app/src/main/java/com/irfancan/testpractice/VisaCardPlatform.java
@@ -0,0 +1,26 @@
+package com.irfancan.testpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class VisaCardPlatform extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getMyVisaCard();
+
+
+    }
+
+
+    private void getMyVisaCard() {
+        Log.v("TEST","VISA CARD RECEIVED");
+    }
+
+
+}
