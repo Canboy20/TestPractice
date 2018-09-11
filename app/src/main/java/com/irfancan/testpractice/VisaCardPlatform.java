bac2108cef6e3f91a77c7c403d1dea96e7c03243
IRFAN CAN <irfancan.atay@starcut.com>
2018-09-12 00:00:02 +0300

    Layout for visa card created

--- app/src/main/java/com/irfancan/testpractice/VisaCardPlatform.java
+++ app/src/main/java/com/irfancan/testpractice/VisaCardPlatform.java
@@ -9,7 +9,7 @@ public class VisaCardPlatform extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.visa_card_layout);
 
 
         getMyVisaCard();
